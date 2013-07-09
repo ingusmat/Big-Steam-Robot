4f34513a8b8c6283cc745600efb92c4ea2af4589
Ingusmat <ingusmat@gmail.com>
2013-07-09 11:57:19 -0400

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,9 @@ Role.create([
   { :name => 'VIP' }
 ], :without_protection => true)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'alejandroplease', :password_confirmation => 'alejandroplease'
-puts 'New user created: ' << user2.name
 user.add_role :admin
-user2.add_role :VIP
 
 puts 'DEFAULT GAMES'
 [{name: 'Warmachine', magic_name: 'focus'}, 
