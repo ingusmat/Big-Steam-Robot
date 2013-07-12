22c75f4c9bd9631c6016e60c86571775d521b66d
Ingusmat <ingusmat@gmail.com>
2013-07-11 23:02:04 -0400

    assets precompiled

--- config/application.rb
+++ config/application.rb
@@ -66,6 +66,6 @@ module BigSteamRobot
     config.assets.version = '1.0'
 
     # for deploying on Heroku
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
   end
 end
